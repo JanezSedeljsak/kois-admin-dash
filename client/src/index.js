801a5fda84a8650ce9e0105ad565c14bd005ea62
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-02-02 09:30:13 +0100

    removed redux...........

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,9 @@ import AppContainer from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-// Add these imports - Step 1
-import { Provider } from 'react-redux';
-import { store } from './redux/store/index';
-
 // Wrap existing app in Provider - Step 2
 ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
+    <AppContainer />,
   document.getElementById('root')
 );
 
--- client/src/redux/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const activateGeod = geod => ({
-  type: "ACTIVATE_GEOD",
-  geod
-});
-
-export const closeGeod = () => ({
-  type: "CLOSE_GEOD"
-});
