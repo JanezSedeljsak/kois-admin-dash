1a770008f11799beef3a39a76569d552804344a0
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-02-19 17:21:44 +0100

    version update

---
+++ client/build/precache-manifest.a002ccfd1c48c6824e941b6715cef86b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6f64233fa5382be11275cbdf9902a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf8ad973dd84bc986f1",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "4a0e1f287ffa126a1a1c",
+    "url": "/static/css/main.0614a087.chunk.css"
+  },
+  {
+    "revision": "ddf8ad973dd84bc986f1",
+    "url": "/static/js/2.2d4230c3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.2d4230c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a0e1f287ffa126a1a1c",
+    "url": "/static/js/main.24ac58da.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "e5789706830b603cc3abcfb22cdeca34",
+    "url": "/static/media/koislogo.e5789706.png"
+  },
+  {
+    "revision": "001b21b615c532add9674adaf952a4bf",
+    "url": "/static/media/scvlogo.001b21b6.png"
+  }
+]);
