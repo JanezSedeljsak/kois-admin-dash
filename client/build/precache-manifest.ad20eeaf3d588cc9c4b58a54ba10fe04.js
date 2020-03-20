d23544a7a6528e13b50f9113da81616d3b6b5541
JanezSedeljsak <janez.sedeljsak@gmail.com>
2020-03-20 16:27:28 +0100

    build fix

---
+++ client/build/precache-manifest.ad20eeaf3d588cc9c4b58a54ba10fe04.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac5281f6abedba298cd9e3b07a5ef59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c290daefd0e91a80ea4",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "d7b696b0e0dc13264fd2",
+    "url": "/static/css/main.66a602c1.chunk.css"
+  },
+  {
+    "revision": "5c290daefd0e91a80ea4",
+    "url": "/static/js/2.19ba9f11.chunk.js"
+  },
+  {
+    "revision": "83ed9e4d5ac5b638642dfc3adad2802a",
+    "url": "/static/js/2.19ba9f11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b696b0e0dc13264fd2",
+    "url": "/static/js/main.e1fcae45.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "4d70ee5d048241debf63e7d373920795",
+    "url": "/static/media/kois-welcome.4d70ee5d.png"
+  },
+  {
+    "revision": "e5789706830b603cc3abcfb22cdeca34",
+    "url": "/static/media/koislogo.e5789706.png"
+  },
+  {
+    "revision": "001b21b615c532add9674adaf952a4bf",
+    "url": "/static/media/scvlogo.001b21b6.png"
+  },
+  {
+    "revision": "a9210b592313272532e72c62062e43a0",
+    "url": "/static/media/user.a9210b59.png"
+  }
+]);
